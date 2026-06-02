0e263aa810b0f2f6ceef16dbbf1f3cac5c7c754b
Argha Sen <53963944+ArghaSenn@users.noreply.github.com>
2026-06-02 11:33:26 +0000

    Created a docker pipeline for the whole process

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-# Base image of the container
-FROM python:3.12.1-slim
-
-# Copying uv from the virtual env
-COPY --from=ghcr.io/astral-sh/uv:latest /uv/ bin
--- Pipeline/pipeline/docker_run.sh
+++ Pipeline/pipeline/docker_run.sh
@@ -1,19 +1,43 @@
 docker run -it --rm \
+    --network=pg-network \
     -e POSTGRES_USER="root" \
     -e POSTGRES_PASSWORD="root" \
     -e POSTGRES_DB="ny_taxi" \
     -v ny_taxi_postgres_data:/var/lib/postgresql \
     -p 5432:5432 \
+    --name pgdatabase \
     postgres:18
 
 
-uv run python ingest_data.py \
+# uv run python ingest_data.py \
+#     --pg-user=root \
+#     --pg-password=root \
+#     --pg-host=localhost \
+#     --pg-port=5432 \
+#     --pg-database=ny_taxi \
+#     --pg-target-table=yellow_taxi_data \
+#     --year=2020 \
+#     --month=2 \
+#     --chunksize=100000
+
+docker run -it --rm \
+    --network=pg-network \
+    taxi_ingest:v001 \
     --pg-user=root \
     --pg-password=root \
-    --pg-host=localhost \
+    --pg-host=pgdatabase \
     --pg-port=5432 \
     --pg-database=ny_taxi \
     --pg-target-table=yellow_taxi_data \
     --year=2020 \
     --month=2 \
     --chunksize=100000
+
+docker run -it --rm \
+    --network=pg-network \
+    -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+    -e PGADMIN_DEFAULT_PASSWORD="root" \
+    -v pgadmin_data:/var/lib/pgadmin \
+    -p 8085:80 \
+    --name pgadmin \
+    dpage/pgadmin4
